f4081670d589a9bd67b47841917bf207febd9e7e
dangroze <dangroze@gmail.com>
2019-05-10 19:46:46 +0100

    :white_check_mark:Update tests

--- spec/gildedrose_spec.rb
+++ spec/gildedrose_spec.rb
@@ -1,4 +1,5 @@
 require 'gildedrose'
+require 'item'
 
 describe GildedRose do
 
@@ -7,6 +8,17 @@ describe GildedRose do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
+    end
+    it "lowers the sell_in by one after a day" do
+      item = Item.new("item", 1, 0)
+      GildedRose.new([item]).update_quality
+      expect(item.sell_in).to eq 0
+    end
+
+    it "never lowers quality below 0" do
+      item = Item.new("item", 0, 0)
+      GildedRose.new([item]).update_quality
+      expect(item.quality).to eq(0)
     end
   end
 
